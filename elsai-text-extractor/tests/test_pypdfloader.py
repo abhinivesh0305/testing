a26c2ecd326e69a51c395785639610e42d264ed5
abhinivesh0305 <abhinivesh.s@optisolbusiness.com>
2025-07-01 22:29:37 +0530

    dfghdf

--- elsai-text-extractor/tests/test_pypdfloader.py
+++
@@ -1,9 +0,0 @@
-from elsai_text_extractors.pypdfloader import PyPDFTextExtractor
-import os
-
-def test_pypdfloader_service():
-    file_name = os.path.join(os.path.dirname(os.path.dirname(__file__)), "test_files", "globalwarming.pdf")
-    pypdfloader_service = PyPDFTextExtractor(file_name)
-    expected_text = "Global warming \nGlobal warming is the long-term rise in Earth's average temperature, primarily driven by \nhuman activities like burning fossil fuels, leading to climate change and environmental \nimpacts. "
-    extracted_text = pypdfloader_service.extract_text_from_pdf()
-    assert extracted_text == expected_text
--- elsai-text-extractor/tests/test_unstructured_excel_loader.py
+++
@@ -1,8 +0,0 @@
-from elsai_text_extractors.unstructured_excel_loader import UnstructuredExcelLoaderService
-import os
-def test_unstructured_excel_loader_service():
-    file_name = os.path.join(os.path.dirname(os.path.dirname(__file__)), "test_files", "weatherdata.xlsx")
-    unstructured_excel_loader_service = UnstructuredExcelLoaderService(file_name)
-    expected_text = '\n\n\ndate\ntemperature\n\n\n2013-01-01 00:00:00\n10\n\n\n2013-01-02 00:00:00\n7.4\n\n\n2013-01-03 00:00:00\n7.166667\n\n\n2013-01-04 00:00:00\n8.666667\n\n\n2013-01-05 00:00:00\n6\n\n\n2013-01-06 00:00:00\n7\n\n\n2013-01-07 00:00:00\n7\n\n\n2013-01-08 00:00:00\n8.857143\n\n\n2013-01-09 00:00:00\n14\n\n\n'
-    documents = unstructured_excel_loader_service.load_excel()
-    assert documents[0].page_content == expected_text
